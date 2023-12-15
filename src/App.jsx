3bbae6c50daa6efcb2cbaad762c4c4359e9dbb88
Talat09 <talat181008312@gmail.com>
2023-12-15 22:00:13 +0600

    set up counter app redux project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl text-center font-bold my-8">
+        Welcome to Counter APP
+      </h1>
+    </>
+  );
+}
+
+export default App;
